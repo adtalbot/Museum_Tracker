d4d60a163c9de0b48d362a71badb93a371918861
audrie <audrie.talbot@gmail.com>
2015-06-12 09:33:38 -0700

    Passes spec for second part of initialize method for museum class.

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -6,5 +6,9 @@ describe(Museum) do
       test_museum = Museum.new({:name => 'Portland Museum', :id => nil})
       expect(test_museum.name()).to(eq('Portland Museum'))
     end
+    it('can be initialized with a database ID') do
+      test_museum = Museum.new({:name => 'Portland Museum', :id => 1})
+      expect(test_museum.id()).to(eq(1))
+    end
   end
 end
