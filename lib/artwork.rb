29954779a1f2ccd83672b784cedb888812cd197f
audrie <audrie.talbot@gmail.com>
2015-06-12 10:31:45 -0700

    Passes spec for all method for artwork class.

--- lib/artwork.rb
+++ lib/artwork.rb
@@ -5,4 +5,15 @@ class Artwork
     @name = attributes.fetch(:name)
     @id = attributes.fetch(:id)
   end
+
+  define_singleton_method(:all) do
+    returned_artwork = DB.exec("SELECT * FROM artworks;")
+    artworks = []
+    returned_artwork.each() do |artwork|
+      name = artwork.fetch('name')
+      id = artwork.fetch('id').to_i()
+      artworks.push(Artwork.new({:name => name, :id => id}))
+    end
+    artworks
+  end
 end
