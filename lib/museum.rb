d7597b16817d87f5615fec2f529cf00ce891d6fb
audrie <audrie.talbot@gmail.com>
2015-06-12 10:00:05 -0700

    Passes specs for save and eql methods for museum class.

--- lib/museum.rb
+++ lib/museum.rb
@@ -5,4 +5,24 @@ class Museum
     @name = attributes.fetch(:name)
     @id = attributes.fetch(:id)
   end
+
+  define_singleton_method(:all) do
+    returned_museums = DB.exec("SELECT * FROM museums;")
+    museums = []
+    returned_museums.each() do |museum|
+      name = museum.fetch('name')
+      id = museum.fetch('id').to_i()
+      museums.push(Museum.new({:name => name, :id => id}))
+    end
+    museums
+  end
+
+  define_method(:save) do
+    result = DB.exec("INSERT INTO museums VALUES ('#{@name}') RETURNING id;")
+    @id = result.first.fetch('id').to_i()
+  end
+
+  define_method(:==) do |another_museum|
+    self.name().==(another_museum.name()).&(self.id().==(another_museum.id()))
+  end
 end
--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -11,4 +11,26 @@ describe(Museum) do
       expect(test_museum.id()).to(eq(1))
     end
   end
+
+  describe('.all') do
+    it('is empty at first') do
+      expect(Museum.all()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('adds a museum to the database') do
+      test_museum = Museum.new({:name => 'Portland Museum', :id => nil})
+      test_museum.save()
+      expect(Museum.all()).to(eq([test_museum]))
+    end
+  end
+
+  describe('#==') do
+    it('recognizes that two museums with the same name and id are equal') do
+      test_museum = Museum.new({:name => 'Portland Museum', :id => nil})
+      test_museum2 = Museum.new({:name => 'Portland Museum', :id => nil})
+      expect(test_museum).to(eq(test_museum2))
+    end
+  end
 end
